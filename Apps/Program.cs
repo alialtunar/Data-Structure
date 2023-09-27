c8c95d32ad831b8b89d103884985c53be45d4527
alialtunar <altunarali284@gmail.com>
2023-09-27 15:16:15 +0300

    Add project files.

---
+++ Apps/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Apps
+{
+    class Program
+    {
+        static void Main(string[] args)     
+        {
+                        var arr = new DataStructures.Array.Array<int>();
+           arr.Add(10);
+            arr.Add(20);
+            arr.Add(30);
+            arr.Add(40);
+            arr.Add(50);
+            Console.WriteLine($"{arr.Count} / {arr.Capacity}");
+            arr.Remove();
+            arr.Remove();
+
+            Console.WriteLine($"{arr.Count} / {arr.Capacity}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DataStructures/Array/Array.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Array
+{
+    public class Array<T> : IEnumerable<T>, ICloneable
+    {
+        private T[] InnerList;
+
+        public int Count { get; private set; }
+        public int Capacity => InnerList.Length;
+
+
+        public Array()
+        {
+            InnerList = new T[2];
+            Count = 0;
+        }
+
+        public void Add(T item)
+        {
+            if (InnerList.Length == Count)
+            {
+                DoubleArray();
+
+            }
+            InnerList[Count] = item;
+            Count++;
+        }
+
+        public T Remove()
+        {
+            if (Count == 0)
+                throw new Exception("Data Not Found");
+            if (InnerList.Length / 4 == Count)
+                HalfArray();
+            var temp = InnerList[Count - 1];
+            if (Count > 0)
+                Count--;
+            return temp;
+
+        }
+
+        private void HalfArray()
+        {
+            if (InnerList.Length > 2)
+            {
+                var temp = new T[InnerList.Length / 2];
+                System.Array.Copy(InnerList, temp, InnerList.Length / 4);
+                InnerList = temp;
+            }
+        }
+
+        private void DoubleArray()
+        {
+            var temp = new T[InnerList.Length * 2];
+            System.Array.Copy(InnerList, temp, InnerList.Length);
+            InnerList = temp;
+        }
+
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }   
+    }
+}
